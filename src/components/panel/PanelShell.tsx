7700eaf9d246c530beca5ee2ec5a1b95188cf585
Servingbuilderapp <servingbuilderapp@gmail.com>
2026-09-15 19:53:23 -0500

    Add files via upload

--- src/components/panel/PanelShell.tsx
+++ src/components/panel/PanelShell.tsx
@@ -25,6 +25,7 @@ export function PanelShell({
         abiertoEnMovil={menuMovil}
         onCerrar={() => setMenuMovil(false)}
         esEquipo={rolUsuario === 'Administrador'}
+        esSocio={rolUsuario === 'Socio'}
       />
 
       <div className="flex-1 flex flex-col min-w-0">
